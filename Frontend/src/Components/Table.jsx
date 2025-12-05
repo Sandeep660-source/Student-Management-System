df2a16a41e9d37e8839cd63484945918ee9930c8
Sandeep660-source <kssandeep8007@gmail.com>
2025-12-05 17:55:07 +0530

    first commit

---
+++ Frontend/src/Components/Table.jsx
@@ -0,0 +1,76 @@
+import React from 'react'
+
+
+
+const Table = () => {
+  return (
+    <div>
+         
+    <div>
+      <table>
+        <thead>
+          
+            <tr>
+              <th>Name</th>
+              <th>Age</th>
+              <th>Action</th>
+            
+
+            </tr>
+          
+        </thead>
+        <tbody>
+          <tr>
+            <td>Shashank</td>
+            <td>25</td>
+            <td>
+                <div>
+                    <button>Edit</button>
+                <button>Delete</button>
+                </div>
+            </td>
+          </tr>
+           <tr>
+            <td>Manish</td>
+            <td>23</td>
+            <td><div>
+                    <button>Edit</button>
+                <button>Delete</button>
+                </div></td>
+          </tr>
+           <tr>
+            <td>farooq</td>
+            <td>24</td>
+            <td><div>
+                    <button>Edit</button>
+                <button>Delete</button>
+                </div></td>
+          </tr>
+           <tr>
+            <td>Manya</td>
+            <td>21</td>
+            <td><div>
+                    <button>Edit</button>
+                <button>Delete</button>
+                </div></td>
+          </tr>
+           <tr>
+            <td>Sandeep</td>
+            <td>21</td>
+            <td><div>
+                    <button>Edit</button>
+                <button>Delete</button>
+                </div></td>
+          </tr>
+        </tbody>
+      </table>
+    </div>
+      
+    </div>
+    
+      
+       
+  )
+}
+
+export default Table
