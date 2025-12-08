51c74f9773d74276e3e6edb59acc12355041178f
Sandeep660-source <kssandeep8007@gmail.com>
2025-12-08 18:51:18 +0530

    update

--- Backend/server.js
+++ Backend/server.js
@@ -4,6 +4,30 @@ const express = require("express")
 const app= express()
 const PORT = 3000;
 
+const mongoose = require("mongoose")
+const Student = require("./model/Student")
+
+
+
+app.post("/api/Student",async(req,res)=>{
+    try{
+        const newStudent = req.body;
+
+        const Student = new Student(newStudent);
+        await Student.save();
+
+        res.statusMessage(200).json({
+            success : true,
+            student: Student
+        })
+
+    }
+    catch(error){
+        console.log("Error Creating Student")
+
+    }
+
+})
 app.listen(PORT,()=>{
     console.log("Application is running in Port",PORT)
 })
